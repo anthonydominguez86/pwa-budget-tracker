6b6e6afe633d711d55f605c06ca7c06ab3919553
anthonydominguez86 <adominguez11186@gmail.com>
2021-04-25 11:52:22 -0400

    updated service worker with DATA_CACHE

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,28 +5,30 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "./manifest.webmanifest"
   ];
 
-  const CACHE_NAME= "static"
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener("install", function(evt) {
+// install
+self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-        return cache.addAll(FILES_TO_CACHE);
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
     })
-    );
-    
-  console.log("Install");
+  );
+
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function(evt) {
+// activate
+self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
-          if (key !== CACHE_NAME && key !== CACHE_NAME) {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
           }
@@ -38,12 +40,14 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
-self.addEventListener("fetch", function(evt) {
+// fetch
+self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
+            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
@@ -51,6 +55,7 @@ self.addEventListener("fetch", function(evt) {
             return response;
           })
           .catch(err => {
+            // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
@@ -58,12 +63,17 @@ self.addEventListener("fetch", function(evt) {
 
     return;
   }
-  evt.respondWith(
-      caches.match(evt.request).then(function(response) {
-          return response || fetch(evt.request);
-        })
-        );
-    });
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-    
 
+  evt.respondWith(
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
